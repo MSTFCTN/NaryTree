990f4b3d96c8aa637a57ab2d2b4a4c35ec055bd1
MSTFCTN <mustafaerenofficial@gmail.com>
2026-04-13 22:14:16 +0300

    I created the stack and queue

--- NaryTreeAttempt2/NaryTreeAttempt2/DataStructures.cs
+++ NaryTreeAttempt2/NaryTreeAttempt2/DataStructures.cs
@@ -15,6 +15,7 @@ namespace NaryTreeAttempt2
             private T _value;
             private List<Node<T>> childrens;
             public Node<T> next;
+            public Node<T> prev;
 
             public Node(T value)
             {
@@ -29,12 +30,11 @@ namespace NaryTreeAttempt2
                     return _value;
                 }
                 set
-                {                    
+                {
                     _value = value;
                 }
-            }        
+            }
         }
-
 
         public class List<T>
         {
@@ -48,9 +48,9 @@ namespace NaryTreeAttempt2
             }
 
 
-            public void Add(Node<T> node)
+            public void Add(T item)
             {
-                Node<T> newNode = node;
+                Node<T> newNode = new Node<T>(item);
                 if (head == null)
                 {
                     head = newNode;
@@ -59,43 +59,52 @@ namespace NaryTreeAttempt2
                 else
                 {
                     tail.next = newNode;
-                    tail = newNode;
+                    newNode.prev = tail;
+                    tail = newNode;                    
                 }
-            }     
+            }
 
             public void RemoveLast()
             {
-                if (head == null) { return; }
+                if (head == null) { throw new InvalidOperationException("List is empty."); }
                 if (head == tail)
                 {
                     head = null;
                     tail = null;
                     return;
                 }
-                Node<T> current = head;
-                while (current.next != tail)
-                {
-                    current = current.next;
-                }
-                current.next = null;
-                tail = current;
+                tail.prev.next = null;
+                tail = tail.prev;
             }
 
-            public void Remove(Node<T> node)
+            public void Remove(T item)
             {
-                if(head == null) { return; }
-                if (head == node)
+                if (head == null) { return; }
+                if (head == tail && EqualityComparer<T>.Default.Equals(head.Value, item))
                 {
                     head = null;
                     tail = null;
                     return;
                 }
+                if (EqualityComparer<T>.Default.Equals(head.Value, item))
+                {
+                    head = head.next;
+                    if (head != null)
+                    {
+                        head.prev = null;
+                    }                 
+                    return;
+                }
                 Node<T> current = head;
                 while (current.next != null)
                 {
-                    if (current.next == node)
+                    if (EqualityComparer<T>.Default.Equals(current.next.Value, item))
                     {
-                        current.next = current.next.next;
+                        if(current.next.next != null)
+                        {
+                            current.next.next.prev = current;
+                        }
+                        current.next = current.next.next;                       
                         if (current.next == null)
                         {
                             tail = current;
@@ -119,5 +128,124 @@ namespace NaryTreeAttempt2
             }
 
         }
+
+        public class Stack<T>
+        {
+
+            Node<T> Top;
+            private int _count;
+
+            public Stack()
+            {
+                Top = null;
+            }
+
+
+            public int Count
+            {
+                get
+                {
+                    return _count;
+                }
+                private set
+                {
+                    _count = value;
+                }
+            }
+
+
+            public void Push(T item)
+            {
+                Node<T> newNode = new Node<T>(item);
+                newNode.next = Top; // Top null olsa bile sorunsuz çalışır.
+                Top = newNode;
+                Count++;
+            }
+
+            public T Pop()
+            {
+                if (Top == null) { throw new InvalidOperationException("Stack is empty."); }
+                T value = Top.Value;
+                Top = Top.next;
+                Count--;
+                return value;
+            }
+
+
+            public T Peek()
+            {
+                if (Top == null) { throw new InvalidOperationException("Stack is empty."); }
+                return Top.Value;
+            }
+
+
+            public bool IsEmpty()
+            {
+                return Top == null;
+            }
+        }
+
+        public class Queue<T>
+        {
+            Node<T> head;
+            Node<T> tail;
+            private int _count;
+
+            public Queue()
+            {
+                head = null;
+                tail = null;
+                _count = 0;
+            }
+
+            public int Count
+            {
+                get { return _count; }
+                private set { _count = value; }
+            }
+
+            public void Enqueue(T item)
+            {
+                Node<T> newNode = new Node<T>(item);
+                if (head == null)
+                {
+                    head = newNode;
+                    tail = newNode;
+                }
+                else
+                {
+                    tail.next = newNode;
+                    tail = newNode;
+                }
+                Count++;
+            }
+
+            public T Dequeue()
+            {
+                if (head == null) { throw new InvalidOperationException("Queue is empty."); }
+                T value = head.Value;
+                head = head.next;
+                if (head == null)
+                {
+                    tail = null;
+                }
+                Count--;
+                return value;
+            }
+
+            public T Peek()
+            {
+                if (head == null) { throw new InvalidOperationException("Queue is empty."); }
+                return head.Value;
+            }
+
+            public bool IsEmpty()
+            {
+                return head == null;
+            }
+        }
+
+
+
     }
 }
